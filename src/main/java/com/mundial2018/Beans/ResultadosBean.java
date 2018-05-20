e44c11558f9a2adebf47935d942e3b831bf37887
ariel <brenes989@gmail.com>
2018-05-19 22:58:02 -0600

    Added the entity factoria for getting the entitymanager factory

---
+++ src/main/java/com/mundial2018/Beans/ResultadosBean.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mundial2018.Beans;
+
+import com.mundial2018.Controller.ResultadoJpaController;
+import com.mundial2018.Database.Entities.Resultado;
+import com.mundial2018.Database.Persistance.EntityManagerFactoria;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author ariel
+ */
+@ManagedBean
+@SessionScoped
+public class ResultadosBean implements Serializable {
+
+       private List<Resultado> listResultados;
+       private ResultadoJpaController rjc;
+
+    public ResultadosBean() {
+    listResultados = new ArrayList<Resultado>();
+       EntityManagerFactoria aux = new EntityManagerFactoria();
+
+        EntityManagerFactory emf = aux.getEMF();
+
+    rjc = new ResultadoJpaController(emf);
+    }
+    
+       
+       
+       
+      @PostConstruct
+    public void init() {
+       
+        
+    }
+}
+    
+    
+    
+    
+    
+    
+}
