e6ff427adc099dabc3d961e38c54a38da61407bc
William Víquez Quirós <wvq1986@gmail.com>
2018-06-10 18:37:06 -0600

    * Actualizado y validado correctamente el método subirArchivo

--- src/main/java/com/mundial2018/Controller/empleadosController.java
+++ src/main/java/com/mundial2018/Controller/empleadosController.java
@@ -10,7 +10,6 @@ import com.mundial2018.Database.Entities.Empleado;
 import com.mundial2018.Database.Entities.Login;
 import com.mundial2018.Database.Persistance.EmpleadoDBA;
 import com.mundial2018.Database.Persistance.EntityManagerFactoria;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -21,10 +20,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.persistence.Query;
-import org.primefaces.PrimeFaces;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 
@@ -33,14 +31,13 @@ import org.primefaces.model.UploadedFile;
  * @author ariel
  */
 @ManagedBean
-@RequestScoped
+@ViewScoped 
 public class empleadosController implements Serializable {
-
     private UploadedFile file;
-    private EntityManagerFactoria facto;
+    private final EmpleadoDBA empDB;
+    private final EntityManagerFactoria facto;
     //private Empleado empleado;
-    List<Empleado> emp = new ArrayList<Empleado>();
-
+    List<Empleado> listaEmpleados = new ArrayList<>();
     private Login login;
 
     /**
@@ -48,21 +45,17 @@ public class empleadosController implements Serializable {
      */
     public empleadosController() {
         facto = new EntityManagerFactoria();
-        //empleado = new Empleado();
+        empDB = new EmpleadoDBA();
         login = new Login();
-
     }
 
     @PostConstruct
     public void init() {
-        EmpleadoDBA empDB = new EmpleadoDBA();
-        emp = empDB.getAllEmpleados();
+        listaEmpleados = empDB.getAllEmpleados();
     }
 
     public List<Empleado> getAllEmpleados() {
-
-        return emp;
-
+        return listaEmpleados;
     }
 
     public List<String> getAllEmpleados2() {
@@ -70,80 +63,59 @@ public class empleadosController implements Serializable {
         aux.add("test");
 
         return aux;
-
     }
 
-    /*Crea en la base de datos empleados basados en los archivos que se subieron en el excel.     
+    /**
+     * Crea en la base de datos empleados basados en los archivos que se subieron en el excel.
+     * @param event 
      */
     public void subirArchivo(FileUploadEvent event) {
         if (event != null) {
             file = event.getFile();
-
             excelManipulation auxExcel = new excelManipulation();
-
+            FacesMessage message = null;
+            
             try {
-
-                EmpleadoJpaController empController = new EmpleadoJpaController(facto.getEMF());
-                LoginJpaController loginJpaController = new LoginJpaController(facto.getEMF());
-
+                EmpleadoJpaController empleadojc = new EmpleadoJpaController(facto.getEMF());
+                LoginJpaController loginjc = new LoginJpaController(facto.getEMF());
                 ArrayList<HashMap> mapDeEmpleados = auxExcel.getAllRowsFromCVSfile(file.getInputstream());
 
                 for (HashMap mappeo : mapDeEmpleados) {
                     try {
-
-                        Empleado emp = new Empleado();
-                        emp.setEmail(mappeo.get("email").toString());
-                        emp.setNombre(mappeo.get("nombre").toString());
-
-                        Query query1 = empController.getEntityManager().createNamedQuery("Empleado.findByNombre").setParameter("nombre", emp.getNombre());
-
-                        Empleado aux = null;
-                        if (!query1.getResultList().isEmpty()) {
-                            aux = (Empleado) query1.getResultList().get(0);
-                        }
-                        if (aux == null) {
-                            empController.create(emp);
-                        } else {
-                            emp = aux;
-                        }
-
-                        //int test = emp.getId();
-                        Login log = new Login();
-                        log.setRol(mappeo.get("rol").toString());
-                        log.setUsername(mappeo.get("email").toString());
-                        log.setPassword(mappeo.get("codigo").toString());
-                        log.setEmpleado(emp);
-
-                        /* Login aux2=  null;
-                               Query query2 =loginJpaController.getEntityManager().createNamedQuery("Login.findByEmpleadoid").setParameter("empleadoid", emp.getId());
-                        if (query2.getFirstResult()>0) {
-                            aux2 = (Login) query2.getSingleResult();
-                        }
+                        Integer codigo = Integer.parseInt(mappeo.get("codigo").toString());
+                        String username = mappeo.get("usuario").toString();
                         
+                        Empleado auxEmpleado = empleadojc.findEmpleado(codigo);
+                        Login auxLogin = loginjc.findLoginByUser(username);
                         
-                        if (emp !=null) {*/
-                        loginJpaController.create(log);
-
-                        /* }
-                        else{
-                        //ya existe ese login con ese empleado
-                        }*/
+                        if(Objects.isNull(auxEmpleado) && 
+                                Objects.isNull(auxLogin)) {
+                            Empleado empleado = new Empleado();
+                            empleado.setId(codigo);
+                            empleado.setEmail(mappeo.get("email").toString());
+                            empleado.setNombre(mappeo.get("nombre").toString());
+                            empleadojc.create(empleado);
+                            
+                            Login nuevo = new Login();
+                            nuevo.setUsername(mappeo.get("usuario").toString());
+                            nuevo.setPassword(mappeo.get("cedula").toString());
+                            nuevo.setRol(mappeo.get("rol").toString());
+                            nuevo.setEmpleado(empleado);
+                            loginjc.create(nuevo);
+                        }
                     } catch (Exception e) {
                         Logger.getLogger("Hubo un error al subir un empleado a la base de datos: " + e.getMessage());
-
                     }
-
                 }
-
-            } catch (IOException ex) {
-                FacesMessage message = new FacesMessage("Error", file.getFileName() + " No es un archivo valido de excel.");
+                
+                listaEmpleados = empDB.getAllEmpleados();
+                message = new FacesMessage("Archivo '" + file.getFileName() + "' subido exitosamente", null);
+                
+            } catch (Exception ex) {
+                message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "El formato del archivo no es válido", null);
                 Logger.getLogger(empleadosController.class.getName()).log(Level.SEVERE, null, ex);
-                return;
             }
-
-            PrimeFaces.current().executeScript("PF('listEmployees').update();");
-
-            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+            
             FacesContext.getCurrentInstance().addMessage(null, message);
         }
     }
@@ -179,23 +151,21 @@ public class empleadosController implements Serializable {
                 login.setEmpleado(aux);
                 login.setUsername(aux.getNombre());
                 LoginDBA.create(login);
-
+                listaEmpleados.add(aux);
             } catch (Exception ex) {
-
                 Logger.getLogger(empleadosController.class.getName()).log(Level.SEVERE, null, ex);
-                FacesMessage message = new FacesMessage("Error, ya existe un empleado con ese Nombre", "ya existe un empleado con ese Nombre");
+                FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ya existe un empleado con ese Nombre", null);
                 FacesContext.getCurrentInstance().addMessage(null, message);
+                
                 return;
             }
-            FacesMessage message = new FacesMessage("Succesful", " Empleado agregado.");
+            
+            FacesMessage message = new FacesMessage("Empleado agregado exitosamente", null);
             FacesContext.getCurrentInstance().addMessage(null, message);
-
-        }
-        else{
-        FacesMessage message = new FacesMessage("Succesful", "Nombre, Codigo o Rol no seleccionado, por favor no deje estos espacios en blanco.");
+        } else{
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Nombre, Codigo o Rol no seleccionado, por favor no deje estos espacios en blanco", null);
             FacesContext.getCurrentInstance().addMessage(null, message);
         }
-
     }
 
     public void setFile(UploadedFile file) {
